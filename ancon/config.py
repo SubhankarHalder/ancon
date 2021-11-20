73dfd3e7ccaa28d8d6676f24d7e9d28d13a20afe
SubhankarHalder <subhankarvirgoan@gmail.com>
2021-11-20 16:13:49 +0530

    Removed Key check for LabelMeJson

--- ancon/config.py
+++ ancon/config.py
@@ -3,4 +3,3 @@ List of configuration variables
 """
 # Source format maps to a set containing target formats 
 allowed_formats = {'lmj-rect': {'yolo'}}
-lmj_keys = {'shapes', 'imageHeight', 'imageWidth'}
--- ancon/utils.py
+++ ancon/utils.py
@@ -16,21 +16,3 @@ def json_extract(json_path):
     with open(json_path) as f:
         data = json.load(f)
         return data
-
-def check_lmj_keys(lmj_dict):
-    """Check if LabelMeJSON dict contains relevant keys
-
-    Args:
-        lmj_dict (dict): LabelMeJSON dict
-
-    Returns:
-        boolean: True or False
-    """
-    counter = 0
-    for key in lmj_dict.keys():
-        if key in lmj_keys:
-            counter += 1
-    if counter == len(lmj_keys):
-        return True
-    else:
-        return False
