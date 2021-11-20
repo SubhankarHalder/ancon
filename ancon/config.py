b0360b2ae56c8c8c7525f8da2ec1bd45bf1ec1a2
SubhankarHalder <subhankarvirgoan@gmail.com>
2021-11-20 15:38:30 +0530

    Add config file

---
+++ ancon/config.py
@@ -0,0 +1,6 @@
+""" 
+List of configuration variables
+"""
+# Source format maps to a set containing target formats 
+allowed_formats = {'lmj-rect': {'yolo'}}
+lmj_keys = {'shapes', 'imageHeight', 'imageWidth'}
--- ancon/utils.py
+++ ancon/utils.py
@@ -2,6 +2,7 @@
 Helper functions for the library
 """
 import json
+from config import lmj_keys
 
 def json_extract(json_path):
     """Converts JSON file to Python Dict
@@ -15,3 +16,21 @@ def json_extract(json_path):
     with open(json_path) as f:
         data = json.load(f)
         return data
+
+def check_lmj_keys(lmj_dict):
+    """Check if LabelMeJSON dict contains relevant keys
+
+    Args:
+        lmj_dict (dict): LabelMeJSON dict
+
+    Returns:
+        boolean: True or False
+    """
+    counter = 0
+    for key in lmj_dict.keys():
+        if key in lmj_keys:
+            counter += 1
+    if counter == len(lmj_keys):
+        return True
+    else:
+        return False
