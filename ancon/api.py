3a7baf140eb592c0adc8528b9b12d9f3bcded9cc
SubhankarHalder <subhankarvirgoan@gmail.com>
2021-11-21 15:04:48 +0530

    Add core dispatch functions

--- ancon/api.py
+++ ancon/api.py
@@ -2,41 +2,48 @@
 API Endpoints
 """
 from utils import check_input_format, check_names_file_req
+from core import file_lmjrect_yolo, dir_lmjrect_yolo
 
 
-def convert_file(source_format, target_format, source_file, destination_folder,
+def convert_file(src_fmt, tgt_fmt, src_file, dest_dir,
                  names_file=None, img_file=None):
     """Converts annotation file from one format to another
 
     Args:
-        source_format (str): Source annotation format type
-        target_format (str): Target annotation format type
-        source_file (str): Path to the annotation file
-        destination_folder (str): Path to destination format where
+        src_fmt (str): Source annotation format type
+        tgt_fmt (str): Target annotation format type
+        src_file (str): Path to the annotation file
+        dest_dir (str): Path to destination format where
                                   the new annotation file would be saved
         names_file (str): Path to names file that contains list of classes.
                           Required for certain conversions. Defaults to None.
         img_file (str): Path to image file. Required for certain conversions.
                         Defaults to None.
     """
-    check_input_format(source_format, target_format)
-    check_names_file_req(source_format, target_format, names_file)
+    check_input_format(src_fmt, tgt_fmt)
+    check_names_file_req(src_fmt, tgt_fmt, names_file)
+    optional_args = {"names": names_file, "img": img_file}
+    file_dispatcher = {'lmj-rect': {'yolo': file_lmjrect_yolo}}
+    file_dispatcher[src_fmt][tgt_fmt](src_file, dest_dir, optional_args)
 
 
-def convert_folder(source_format, target_format, source_folder,
-                   destination_folder, names_file=None, img_folder=None):
+def convert_folder(src_fmt, tgt_fmt, src_dir,
+                   dest_dir, names_file=None, img_dir=None):
     """Converts annotation files in a folder from one format to another
 
     Args:
-        source_format (str): Source annotation format type
-        target_format (str): Target annotation format type
-        source_file (str): Path to the folder containing the annotations files
-        destination_folder (str): Path to destination format where
+        src_fmt (str): Source annotation format type
+        tgt_fmt (str): Target annotation format type
+        src_dir (str): Path to the folder containing the annotations files
+        dest_dir (str): Path to destination format where
                                   the new annotation files would be saved
         names_file (str): Path to names file that contains list of classes.
                           Required for certain conversions. Defaults to None.
-        img_file (str): Path to folder containing image files.
+        img_dir (str): Path to folder containing image files.
                         Required for certain conversions. Defaults to None.
     """
-    check_input_format(source_format, target_format)
-    check_names_file_req(source_format, target_format, names_file)
+    check_input_format(src_fmt, tgt_fmt)
+    check_names_file_req(src_fmt, tgt_fmt, names_file)
+    optional_args = {"names": names_file, "img": img_dir}
+    dir_dispatcher = {'lmj-rect': {'yolo': dir_lmjrect_yolo}}
+    dir_dispatcher[src_fmt][tgt_fmt](src_dir, dest_dir, optional_args)
--- ancon/core.py
+++ ancon/core.py
@@ -3,6 +3,8 @@ Business Logic
 """
 from pathlib import Path
 from utils import json_extract
+from utils import class_values
+from tqdm import tqdm
 
 
 def pascalvoc_boxconvert_yolo(img_width, img_height, pascalvoc_box):
@@ -31,7 +33,7 @@ def pascalvoc_boxconvert_yolo(img_width, img_height, pascalvoc_box):
     return [x, y, w, h]
 
 
-def extract_lmj_rect_pascalvoc(label_dict):
+def extract_lmjrect_pascalvoc(label_dict):
     """Extracts box coordinates from lmjson labels to pascalvoc
 
     Args:
@@ -48,7 +50,7 @@ def extract_lmj_rect_pascalvoc(label_dict):
     return [x_min, y_min, x_max, y_max]
 
 
-def lmj_rect_yolo(json_file_path, target_dir, class_dict):
+def lmjrect_yolo(json_file_path, target_dir, class_dict):
     stats = {"converted": 0, "skipped": 0}
     json_dict = json_extract(json_file_path)
     img_width = json_dict["imageWidth"]
@@ -61,7 +63,7 @@ def lmj_rect_yolo(json_file_path, target_dir, class_dict):
     for idv_label in label_list:
         if idv_label["shape_type"] == "rectangle":
             yolo_class = class_dict(idv_label["label"])
-            pascalvoc_box = extract_lmj_rect_pascalvoc(idv_label)
+            pascalvoc_box = extract_lmjrect_pascalvoc(idv_label)
             yolo_box = pascalvoc_boxconvert_yolo(img_width, img_height,
                                                  pascalvoc_box)
             yolo_box_str = " ".join([str(val) for val in yolo_box])
@@ -71,3 +73,34 @@ def lmj_rect_yolo(json_file_path, target_dir, class_dict):
         else:
             stats["skipped"] += 1
     return stats
+
+
+def preproces_lmjrect(dest_dir, **kwargs):
+    Path(dest_dir).mkdir(parents=True, exist_ok=True)
+    names_file_path = kwargs["names"]
+    class_dict = class_values(names_file_path)
+    return class_dict
+
+
+def file_lmjrect_yolo(src_file, dest_dir, **kwargs):
+    class_dict = preproces_lmjrect(dest_dir, kwargs)
+    statistics = lmjrect_yolo(src_file, dest_dir, class_dict)
+    converted = statistics["converted"]
+    skip = statistics["skipped"]
+    if converted == 0:
+        print("No Rectangle Type labels found. Created 1 empty file")
+    if skip > 0:
+        print(f"Skipping {skip} labels. LabelMe Rectangle Type not found")
+
+
+def dir_lmjrect_yolo(src_dir, dest_dir, **kwargs):
+    class_dict = preproces_lmjrect(dest_dir, kwargs)
+    empty_file = 0
+    skip = 0
+    for file in tqdm(list(Path(src_dir).iterdir())):
+        if file.suffix() == ".json":
+            temp_stats = lmjrect_yolo(file, dest_dir, class_dict)
+            if temp_stats["converted"] == 0:
+                empty_file += 1
+            if temp_stats["skipped"] == 0:
+                skip += 1
